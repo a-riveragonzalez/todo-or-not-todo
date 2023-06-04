8eb3d35f753039a3ca1862b589f15bdbfafc3f6a
Arely Rivera <arelyrivera1996@gmail.com>
2023-06-03 21:23:21 -0700

    added handle event function

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,31 @@ import "./App.css";
 const App: React.FC = () => {
 
   interface TodoInt {
-    name: string, 
-    priority: number,
-    task: string
+    name: string;
+    priority: number;
+    task: string;
   }
 
-  const [userTodo, setUserTodo] = useState<{currentTodo: TodoInt}>({
+  interface AllTodosInt {
+    currentTodo: TodoInt; 
+    // inside the array there will be objects with the shape of TodoInt
+    allTodos: Array<TodoInt>;
+  }
+
+  const [userTodo, setUserTodo] = useState<AllTodosInt>({
     currentTodo : {
       name: "",
       priority: 0, 
       task: ""
-    }
+    }, 
+    allTodos: []
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) : void => {
     // console.log(event.target.value);
 
     setUserTodo({
+      ...userTodo,
       currentTodo: {
         ...userTodo.currentTodo,
         [event.target.name]: event.target.value,
@@ -29,12 +37,33 @@ const App: React.FC = () => {
     })
   }
 
+  const handleSubmit = (event: React.SyntheticEvent) : void => {
+    event.preventDefault();
+
+    setUserTodo({
+      currentTodo: {
+        name: "",
+        priority: 0, 
+        task: ""
+      },
+      allTodos: [
+        ...userTodo.allTodos, 
+        userTodo.currentTodo,
+      ]
+    })
+  }
+
+  // to add all todo's onto the page 
+  const todosList = userTodo.allTodos.map
+
+  console.log(userTodo)
+
   return (
     <>
       <header>
         <h1>Todo or not Todo</h1>
       </header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name :</label>
         <input 
           id="name" 
